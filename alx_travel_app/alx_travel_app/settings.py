b33666205453f93d0011b50a73c38d0e7204c09f
Yonas Mamo <Yonasma416@gmail.com>
2025-09-07 22:55:38 +0300

    final adjustments for deployment

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TIME_ZONE = 'UTC'
 USE_TZ = True
 
